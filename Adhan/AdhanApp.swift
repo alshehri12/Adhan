1e2b9fe74058b6df5c5878b55fbc1cfa126c0c39
alshehri12 <xmx1000@gmail.com>
2025-05-30 18:39:43 +0300

    Initial Commit

---
+++ Adhan/AdhanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdhanApp.swift
+//  Adhan
+//
+//  Created by Abdulrahman Alshehri on 03/12/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AdhanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adhan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Adhan
+//
+//  Created by Abdulrahman Alshehri on 03/12/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
